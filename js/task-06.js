2cf87a3e56a2c5985e5deb244d35200166585163
AlexanderPetrik <painkiller408@gmail.com>
2023-05-11 22:05:55 +0300

    js-hw-06 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,13 @@
 const textInput = document.querySelector('#validation-input');
 
-const minLength = textInput.getAttribute("data-length");
+const requiredLength = textInput.getAttribute("data-length");
 
 textInput.addEventListener('blur', () => {
-    const isValid = textInput.value.length >= minLength;
+    const isValid = textInput.value.length === +requiredLength;
     const className = isValid ? 'valid' : 'invalid';
     textInput.classList.add(className);
     textInput.classList.remove(isValid ? 'invalid' : 'valid');
 });
+
+
 
--- js/task-07.js
+++ js/task-07.js
@@ -2,8 +2,9 @@ const control = document.querySelector('#font-size-control');
 
 const text = document.querySelector('#text');
 
+text.style.fontSize = `${control.value}px`;
 
-control.addEventListener('change', () => {
+control.addEventListener('input', () => {
     text.style.fontSize = `${control.value}px`;
 });
 
